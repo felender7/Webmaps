154eaea95d4ec94a74f61e76ad96571b96afdbf0
felender <felender7@gmail.com>
2021-04-14 16:54:19 +0200

    Adding Multiple Markers to the Map

--- map.py
+++ map.py
@@ -4,11 +4,16 @@ import folium as f
 map =  f.Map(location = [-28.29148236287175, 23.549727933663252], zoom_start = 6, tiles= "cartodb positron")
 
 # Adding Elements to the map
+coordinates = [[-29.60848569170139, 28.345260248070595], [-23.852348170408053, 28.85612449250099] , [-26.47871416756294, 28.87755514305355]]
+popups = ["Lesotho", "Limpop", "Gauteng"]
+colors = ["red","green", "blue"]
+
 fg = f.FeatureGroup(name="My Map")
-fg.add_child(f.Marker(location=[-29.60848569170139, 28.345260248070595], popup="Lesotho", icon=f.Icon(color="green")))
-fg.add_child(f.Marker(location=[-23.852348170408053, 28.85612449250099], popup="Limpopo", icon=f.Icon(color="red")))
+for coordinate,popup,color in zip(coordinates, popups, colors) :
+    fg.add_child(f.Marker(location=coordinate, popup=popup, icon=f.Icon(color=color)))
 
 map.add_child(fg)
+
 
 #Save the Map
 map.save("south_africa_map.html")
