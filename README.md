f7cc51e56fbe4573509c8de87cc0054a417efaef
felender <felender7@gmail.com>
2021-04-14 21:58:56 +0200

    Adding Readme.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# South Africa Cities Database and Population
+List of 108 prominent cities in South Africa. Each row includes a city's latitude, longitude, province and other variables of interest. This is a subset of all 11,521 places in South.
+Data Source:[https://simplemaps.com/data/za-cities.json]
+
+## Getting Started
+
+Mapping is a Python application.
+
+
+#### Requirements
+
+You'll need the following installed to run the application successfully:
+* Python 3 & Pip3
+* Folium Library - 'pip3 install folium'
+* Pandas Library - 'pip3 install pandas'
--- map.py
+++ map.py
@@ -17,15 +17,22 @@ populations = list(data["population"])
 colors = ['black', 'red', 'green', 'lightgreen', 'darkblue', 'cadetblue', 'darkgreen', 'lightgray', 'darkpurple', 'lightblue', 'blue', 'beige', 'red', 'orange', 'gray', 'pink', 'darkred', 'lightred', 'purple'] * 6
 
 # Adding Elements to the map
-fgp = folium.FeatureGroup(name="Population")
+fgv = f.FeatureGroup(name="SA MAP")
+for latitude, longitude,popup, color,population in zip(lat,lng, popups, colors, populations) :
+    fgv.add_child(f.Marker(location=[latitude, longitude], popup=popup + "\nPopulation: " + str(population) , icon=f.Icon(color=color)))
 
-fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+#Adding a GeoJson Polygon
+
+fgp = f.FeatureGroup(name="Population")
+
+fgp.add_child(f.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fgv)
 map.add_child(fgp)
-map.add_child(folium.LayerControl())
+map.add_child(f.LayerControl())
+
 
 #Save the Map
 map.save("south_africa_map.html")
