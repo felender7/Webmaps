628da65de77dbafb24c8fd2c28b835dc173bdc02
felender <felender7@gmail.com>
2021-04-14 19:54:06 +0200

    Adding a GeoJson Polygon

--- map.py
+++ map.py
@@ -3,24 +3,29 @@ import pandas as pd
 # Create  Map object
 map =  f.Map(location = [-28.29148236287175, 23.549727933663252], zoom_start = 6, tiles= "cartodb positron")
 
-# Adding Elements to the map
+
 # Load data from csv
 data = pd.read_csv("za.csv")
 
-#get lat and lng
+#get lat, lng, City, population
 lat = list(data["lat"])
 lng = list(data["lng"])
 popups = list(data["city"])
 populations = list(data["population"])
+
+# Add Colours
 colors = ['black', 'red', 'green', 'lightgreen', 'darkblue', 'cadetblue', 'darkgreen', 'lightgray', 'darkpurple', 'lightblue', 'blue', 'beige', 'red', 'orange', 'gray', 'pink', 'darkred', 'lightred', 'purple'] * 6
-fg = f.FeatureGroup(name="My Map")
 
-for latitude, longitude,popup, color,population in zip(lat,lng, popups, colors, populations) :
-    fg.add_child(f.Marker(location=[latitude, longitude], popup=popup + "\nPopulation: " + str(population) , icon=f.Icon(color=color)))
+# Adding Elements to the map
+fgp = folium.FeatureGroup(name="Population")
 
-fg.add_child(f.GeoJson(data=(open("world.json", "r", encoding='UTF-8').read())))
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 #Save the Map
 map.save("south_africa_map.html")
